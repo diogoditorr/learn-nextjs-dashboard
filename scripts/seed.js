ca05b081faa65a664de0545ff45b8bffc462b3d6
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2023-12-27 19:10:39 -0300

    Chapter 6 - Setting Up Your Database

--- scripts/seed.js
+++ scripts/seed.js
@@ -20,6 +20,8 @@ async function seedUsers(client) {
       );
     `;
 
+    debugger
+
     console.log(`Created "users" table`);
 
     // Insert data into the "users" table
@@ -52,14 +54,14 @@ async function seedInvoices(client) {
 
     // Create the "invoices" table if it doesn't exist
     const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    customer_id UUID NOT NULL,
-    amount INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date DATE NOT NULL
-  );
-`;
+      CREATE TABLE IF NOT EXISTS invoices (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        customer_id UUID NOT NULL,
+        amount INT NOT NULL,
+        status VARCHAR(255) NOT NULL,
+        date DATE NOT NULL
+      );
+    `;
 
     console.log(`Created "invoices" table`);
 
